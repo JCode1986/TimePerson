57bac25f88287b5769c2cc90c38814a198f97205
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-31 09:31:57 -0700

    initial scaffold

---
+++ Lab11-TimePerson/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab11_TimePerson.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Default home route for home page
+        /// </summary>
+        /// <returns>Generate View</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
